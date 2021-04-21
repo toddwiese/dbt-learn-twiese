104745e76bda7a723d6d7759dc289c07e008eba8
Todd Wiese <todd.wiese@chrobinson.com>
2021-04-21 19:04:48 +0000

    payments model added

---
+++ models/payments.sql
@@ -0,0 +1,21 @@
+with
+
+payments as (
+
+        select * from {{ ref('stg_payments') }}
+
+
+),
+
+pivoted as (
+    select 
+            order_id,
+        {%- for payment_method in ['bank_transfer','coupon','credit_card','gift_card'] %}
+            sum(case when payment_method = '{{ payment_method }}' then amount else 0 end) as {{payment_method}}_amount{% if not loop.last %},{% endif %}
+        {%- endfor %}
+    from payments
+    group by 1
+)
+
+select *
+from pivoted
